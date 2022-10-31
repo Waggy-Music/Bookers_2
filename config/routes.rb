060ba59b5831e693d8f68d6bdfc9d6c553f665db
Yusuke Kashiwagi <114566645+Waggy-Music@users.noreply.github.com>
2022-10-31 11:49:11 +0000

    [ohyo4totyu]

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
 
 
+
   devise_for :users
   root to: "homes#top"
+  get "home/about"=> "homes#about",as: "about"
  # get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
  resources :books do
  resource :favorites,only:[:create, :destroy]
  resources:book_comments,only: [:create, :destroy]
-end
+ end
 
- resources :users,only:[:show,:edit,:update, :index]
- get "home/about"=> "homes#about",as: "about"
- 
+ resources :users  do
+ resource :relationships, only: [:create, :destroy]
+ get 'followings' => 'relationships#followings', as: 'followings'
+ get 'followers' => 'relationships#followers', as: 'followers'
+ end
  
   
 end
