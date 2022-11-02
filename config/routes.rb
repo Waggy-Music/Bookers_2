0c9a75e7550ac65f3b230184710f457dcc09d1ff
Yusuke Kashiwagi <114566645+Waggy-Music@users.noreply.github.com>
2022-11-02 11:48:11 +0000

    [ohyo5finish]

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "home/about"=> "homes#about",as: "about"
  # get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/search' => 'searches#search'
   
  resources :books do
  resource :favorites,only:[:create, :destroy]
